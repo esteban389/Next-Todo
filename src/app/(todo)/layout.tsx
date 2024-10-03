4f037c72cf2fec78e6697326143c094f0ba34e95
Esteban Andres Murcia Acuña <estebandres@proton.me>
2024-10-02 21:54:29 -0500

    Refactor layout component and add toaster component

--- src/app/(todo)/layout.tsx
+++ src/app/(todo)/layout.tsx
@@ -1,38 +1,16 @@
-"use client";
-import { motion, AnimatePresence } from "framer-motion";
 import Sidebar from "@/components/layout/sidebar";
-import { usePathname } from "next/navigation";
-
-const pageVariants = {
-  initial: { opacity: 0, y: 35 },
-  animate: { opacity: 1, y: 0 },
-  exit: { opacity: 0, y: -35 },
-};
+import { Toaster } from "@/components/ui/sonner";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }): React.ReactElement {
-  const pathname = usePathname();
-
   return (
     <div className="flex flex-row items-stretch h-full w-full">
       <Sidebar />
-      <div className="w-full">
-        <AnimatePresence mode="wait">
-          <motion.div
-            key={pathname}
-            initial="initial"
-            animate="animate"
-            variants={pageVariants}
-            transition={{ duration: 0.5 }}
-            className="h-full"
-          >
-            {children}
-          </motion.div>
-        </AnimatePresence>
-      </div>
+      <div className="w-full">{children}</div>
+        <Toaster />
     </div>
   );
 }
